212526b7a98c2a3a9fb37546f0daecea13c37355
SamNibbles <40390123+SamNibbles@users.noreply.github.com>
2018-06-19 12:43:23 +0100

    Added fullscreen mode, and started the player class

--- app/src/main/java/com/example/samnibbles/spacegame/Game.java
+++ app/src/main/java/com/example/samnibbles/spacegame/Game.java
@@ -29,7 +29,13 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //set to full screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
